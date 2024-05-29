d8429bccb920ada9c02e3273c21914ae4ad7a596
IDS-Junsong <junsong.dong@saint-gobain.com>
2024-05-29 17:36:33 +0200

    cm

--- exporters/css/src/content/token.ts
+++ exporters/css/src/content/token.ts
@@ -17,7 +17,7 @@ export function convertedToken(token: Token, mappedTokens: Map<string, Token>, t
   })
   
  if (token.tokenType.toLowerCase().indexOf(TokenType.fontWeight.toLowerCase()) > -1){
-    value = Number.isNaN(parseFloat(value)) ? value : +value 
+    value = Number.isNaN(parseFloat(value)) ? value : Number(value) 
  }
  
   const indentString = " ".repeat(exportConfiguration.indent)
