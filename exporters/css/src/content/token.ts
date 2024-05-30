83a2a09467e565acacaef528dc4905ca1c67958c
IDS-Junsong <junsong.dong@saint-gobain.com>
2024-05-30 11:20:58 +0200

    cm

--- exporters/css/src/content/token.ts
+++ exporters/css/src/content/token.ts
@@ -18,12 +18,35 @@ export function convertedToken(token: Token, mappedTokens: Map<string, Token>, t
       return `var(--${tokenVariableName(t, tokenGroups)})`
     },
   })
-   
-  if (token.tokenType === TokenType.fontFamily) {
-    name = name.startsWith('font-family-font-family-') ? name.replace("font-family-font-family","font-family") : name
+  
+  if (token.tokenType === TokenType.borderWidth) {
+    name = name.startsWith('border-width-') ? name.substring(12) : name
   }
     
-   if (token.tokenType === TokenType.fontWeight) {
+  if (token.tokenType === TokenType.fontSize) {
+    name = name.startsWith('font-size-') ? name.substring(10) : name
+  }
+  if (token.tokenType === TokenType.letterSpacing) {
+    name = name.startsWith('letter-spacing-') ? name.substring(15) : name
+  }
+  if (token.tokenType === TokenType.lineHeight) {
+    name = name.startsWith('line-height-') ? name.substring(12) : name
+  }
+  if (token.tokenType === TokenType.radius) {
+    name = name.startsWith('border-radius-') ? name.substring(14) : name
+  }
+  if (token.tokenType === TokenType.size) {
+    name = name.startsWith('sizing-') ? name.substring(7) : name
+  }
+  if (token.tokenType === TokenType.space) {
+    name = name.startsWith('spacing-') ? name.substring(8) : name
+  }
+
+  if (token.tokenType === TokenType.fontFamily) {
+    name = name.startsWith('font-family-') ? name.substring(12) : name
+  }
+    
+  if (token.tokenType === TokenType.fontWeight) {
     let candidate = +(value?.replaceAll('"', ''))
     value = Number.isNaN(candidate) ? value : candidate;
   }
